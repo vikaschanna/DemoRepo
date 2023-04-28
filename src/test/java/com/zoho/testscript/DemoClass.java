b0155dfc95140dcffb617c205aeb2c28ba51a5d4
vikaschanna <vikassschanna72@gmail.com>
2023-04-28 08:29:35 +0530

    2nd commit

---
+++ src/test/java/com/zoho/testscript/DemoClass.java
@@ -0,0 +1,10 @@
+package com.zoho.testscript;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to eclipse");
+	}
+
+}
