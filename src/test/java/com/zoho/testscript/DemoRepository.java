acc4864e501873567427e814b602ebfc436e9fc7
vikaschanna <vikassschanna72@gmail.com>
2023-04-28 08:36:11 +0530

    3rd commit

---
+++ src/test/java/com/zoho/testscript/DemoRepository.java
@@ -0,0 +1,12 @@
+package com.zoho.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoRepository {
+	@Test
+	public void Repository() {
+		Reporter.log("welcome to github");
+//		System.out.println("good morning");
+	}
+}
