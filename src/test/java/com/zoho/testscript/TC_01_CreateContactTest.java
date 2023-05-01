61b47c8ea1acf8dbc494d493708c5be0a89c888c
vikaschanna <vikassschanna72@gmail.com>
2023-05-01 08:42:53 +0530

    commiting

---
+++ src/test/java/com/zoho/testscript/TC_01_CreateContactTest.java
@@ -0,0 +1,10 @@
+package com.zoho.testscript;
+
+public class TC_01_CreateContactTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+	}
+
+}
